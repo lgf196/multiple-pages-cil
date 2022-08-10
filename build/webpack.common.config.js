6e2a38cfcceed14cc543a3f830a70f17cfa26a51
lgf196 <2219014328@qq.com>
2022-08-10 18:08:33 +0800

    公共的scss模块

--- build/webpack.common.config.js
+++ build/webpack.common.config.js
@@ -41,8 +41,12 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
+          'sass-loader',
           {
-            loader: 'sass-loader',
+            loader: 'sass-resources-loader',
+            options: {
+              resources: [path.resolve(__dirname, '../src/style/variable.scss')],
+            },
           },
         ],
       },
--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-import { random } from "@/utils";
-console.log("我是首页大大", random());
+import { random } from '@/utils';
+import './index.scss';
+
+$(document).ready(function () {
+  console.log('我是首页大大', random());
+  mini.parse();
+  var grid = mini.get('datagrid1');
+  grid.load();
+
+  function onSearch() {
+    mini.open({
+      url: bootPATH + '../demo/CommonLibs/SelectGridWindow.html',
+    });
+  }
+});
